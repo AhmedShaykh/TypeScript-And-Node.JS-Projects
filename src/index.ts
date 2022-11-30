f9e7a671eab4ece1e50dc528e2482c64e239e381
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-30 14:50:25 +0500

    Git Init

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,85 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 
-console.log(chalkAnimation.rainbow("Let's Start"));
+const runAnimation = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    })
+};
 
-let input1 = await inquirer.prompt([{
-    name: "number1",
-    type: "number",
-    message: "Enter your Number 1:"
-}
-]);
+async function welcome() {
+    let animation = chalkAnimation.rainbow("Let's Start Calculation");
+    await runAnimation();
 
-let input2 = await inquirer.prompt([{
-    name: "number2",
-    type: "number",
-    message: "Enter your Number 2:"
-}
-]);
+    animation.stop();
 
-let operator = await inquirer.prompt([{
-    name: "number1",
-    type: "number",
-    message: "Enter your Number 1:"
-}
-]);
+    question();
+};
+
+welcome();
+
+async function question() {
+
+    const operators = await inquirer.prompt([
+        {
+            name: 'operator',
+            type: 'list',
+            message: 'Please Select your Operator',
+            choices: [
+                'Addition',
+                'Subtraction',
+                'Multiplication',
+                'Division',
+                'Power',
+            ],
+        },
+        {
+            name: 'number1',
+            type: 'number',
+            message: 'Enter Your Number 1',
+            default() {
+                return 0;
+            },
+        },
+        {
+            name: 'number2',
+            type: 'number',
+            message: 'Enter Your Number 2',
+            default() {
+                return 0;
+            },
+        },
+
+    ]);
+
+    return await calculation(operators.operator, operators.number1, operators.number2);
+};
+
+const calculation = async (isOperator: string, number1: number, number2: number) => {
+    let result: number = 0;
+
+    switch (isOperator) {
+        case "Addition":
+            result = number1 + number2;
+            console.log(chalk.cyan(`Your Result ${number1} + ${number2} = ${result}`));
+            break;
+        case "Subtraction":
+            result = number1 - number2;
+            console.log(chalk.cyan(`Your Result ${number1} - ${number2} = ${result}`));
+            break;
+        case "Multiplication":
+            result = number1 * number2;
+            console.log(chalk.cyan(`Your Result ${number1} * ${number2} = ${result}`));
+            break;
+        case "Division":
+            result = number1 / number2;
+            console.log(chalk.cyan(`Your Result ${number1} / ${number2} = ${result}`));
+            break;
+        case "Power":
+            result = number1 ** number2;
+            console.log(chalk.cyan(`Your Result ${number1} ** ${number2} = ${result}`));
+            break;
+        default:
+            return;
+    }
+};
